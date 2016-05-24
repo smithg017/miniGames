53ab72cd9cf449c36c51b86f8a5e0fa7e4336348
smithg017 <smithg017@byu.net>
2016-05-24 14:22:09 -0400

    First commit

---
+++ Endless2D/src/components/Player.java
@@ -0,0 +1,19 @@
+package components;
+
+public class Player 
+{
+	private int x, y;
+	double velX, velY;
+	private int width, height;
+	
+	public Player()
+	{
+		x = 10;
+		y = 10;
+		velX = 10;
+		velY = 10;
+		width = 20;
+		height = 20;
+	}
+	public Player(int locX, int locY, double vX, double vY, double w,double h)
+}
